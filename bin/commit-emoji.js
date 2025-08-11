adb1d12a7c7bbbb1d28e3b6e25675ad90772bcc1
Davide Cose <davide.cose2007@gmail.com>
2025-08-11 13:29:24 +0200

    Fixed hook installation script

--- bin/commit-emoji.js
+++ bin/commit-emoji.js
@@ -6,13 +6,15 @@ const emojiMap = require("../lib/emojiMap");
 const args = process.argv.slice(2);
 
 if (args[0] === "init") {
-  const hookPath = path.join(process.cwd(), ".git", "hooks", "prepare-commit-msg");
-  const hookContent = `#!/bin/sh
-node ${path.join(__dirname, "../lib/hookHandler.js")} "$1" "$2" "$3"
-`;
-  fs.writeFileSync(hookPath, hookContent, { mode: 0o755 });
-  console.log("✅ Commit-Emoji hook installed!");
-  process.exit(0);
+    const hookPath = path.join(process.cwd(), ".git", "hooks", "prepare-commit-msg");
+    const hookHandlerPath = path.resolve(__dirname, "../lib/hookHandler.js");
+    const hookContent = `#!/bin/sh
+    "${process.execPath}" "${hookHandlerPath}" "$1" "$2" "$3"
+    `;
+
+    fs.writeFileSync(hookPath, hookContent, { mode: 0o755 });
+    console.log("✅ Commit-Emoji hook installed!");
+    process.exit(0);
 }
 
 console.log("Usage: commit-emoji init");
